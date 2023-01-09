93027770d5892896f739e466bba797f32c857b69
Manav Patel <patema01@luther.edu>
2023-01-09 13:03:54 -0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace IT481_Patel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.numOfCustomers = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(300, 302);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(140, 54);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // numOfCustomers
+            // 
+            this.numOfCustomers.AutoSize = true;
+            this.numOfCustomers.Location = new System.Drawing.Point(112, 78);
+            this.numOfCustomers.Name = "numOfCustomers";
+            this.numOfCustomers.Size = new System.Drawing.Size(114, 13);
+            this.numOfCustomers.TabIndex = 1;
+            this.numOfCustomers.Text = "Number of Customers: ";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(115, 94);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 262);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.numOfCustomers);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label numOfCustomers;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT481_Patel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\v11.0;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=true");
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        List<String> data = new List<String>();
+        string numberofCustomers;
+        private  void populateData()
+        {
+            try
+            {
+                string query = "SELECT ContactName FROM [dbo].[Customers]";
+                string query2 = "SELECT COUNT(*) as 'Number Of Customers' FROM [dbo].[Customers]";
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                SqlCommand command2 = new SqlCommand(query2, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                SqlDataReader reader2 = command2.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    data.Add(reader["ContactName"].ToString());
+                }
+                reader.Close();
+                while (reader2.Read())
+                {
+                    numberofCustomers =  reader2["Number Of Customers"].ToString();
+                }
+                conn.Close();
+
+                reader2.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            populateData();
+            numOfCustomers.Text = numOfCustomers.Text + numberofCustomers;
+            foreach(var item in data)
+            {
+                listView1.Items.Add(item.Split(' ')[1]);
+            }
+        }
+
+
+
+    }
+}
